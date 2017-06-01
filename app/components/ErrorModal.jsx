5a1a4ab54615883cc30b767838aa5afaa2227b3b
Derrick <stjohndlee@gmail.com>
2017-06-01 16:49:20 +0900

    fixed an error in ErrorModal.jsx

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server');
 
 var ErrorModal = React.createClass({
   propTypes: {
@@ -12,14 +14,9 @@ var ErrorModal = React.createClass({
   },
 
   componentDidMount: function() {
-    var modal = new Foundation.Reveal($('#error-modal'));
-    modal.open();
-  },
-
-  render: function() {
     var {title, message} = this.props;
 
-    return (
+    var modalMarkup = (
       // Great question! If we add data-close to the JSX the resulting HTML
       // attribute will be data-close="true" . This transformation is made when
       // compiling the JSX. If you add data-close=""  to the JSX, the resulting
@@ -33,6 +30,27 @@ var ErrorModal = React.createClass({
           </button>
         </p>
       </div>
+    );
+
+    // If a string is passed as the parameter to $(), jQuery examines the string
+    // to see if it looks like HTML (i.e., it starts with <tag ... >). If not, the
+    // string is interpreted as a selector expression, as explained above. But if
+    // the string appears to be an HTML snippet, jQuery attempts to create new DOM
+    // elements as described by the HTML. Then a jQuery object is created and
+    // returned that refers to these elements. You can perform any of the usual
+    // jQuery methods on this object:
+
+    // creating a new jQuery object
+    var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+    $(ReactDOM.findDOMNode(this)).html($modal); // appending
+
+    var modal = new Foundation.Reveal($('#error-modal'));
+    modal.open();
+  },
+
+  render: function() {
+    return (
+      <div></div>
     );
   }
 });
